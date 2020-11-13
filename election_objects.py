589534961f576c3e266aa9ad72cdd82d1e842f9e
unplannedlunch <jonah.wolmark@gmail.com>
2020-11-12 20:00:15 -0500

    Added file reading and population

--- election_objects.py
+++ election_objects.py
@@ -1,14 +1,41 @@
+import csv
+import re
+
 class CountryData:
 	def __init__(self):
 		self.states = []
 		self._populate()
 
 	def _populate(self):
-		#Read from data file and store in this class
-		print()
+		#  Read from data file and store in this class
+		with open('2020-presidential.csv', newline = '') as file:
+			csvReader = csv.reader(file)
+			for row in csvReader:
+
+				#  Discards header row and any row that contains a candidate other than Biden or Trump, and a forecast date that is not the most recent in the file. Also if it is US as state.
+				if(row[1] == 'US' or (re.search('forecast', row[0]) or not (re.search('10/06/20', row[0]) and (re.search('Biden', row[4]) or re.search('Trump', row[4]))))):
+					continue
+
+				currentState = None
+				for state in self.states:
+					if state.name == row[1]:
+						currentState = state
+						currentState._setBasePrediction(currentState.getBasePrediction() - float(row[8]))
+						break
+
+				#  Ensures that currentState is never null
+				if currentState is None:
+					currentState = State(row[1], int(row[2]), float(row[8]))
+					self.states.append(currentState)
 
 class State:
 	def __init__(self, name, ev, basePrediction):
 		self.name = name
 		self.ev = ev
 		self.basePrediction = basePrediction
+	
+	def getBasePrediction(self):
+		return self.basePrediction
+	
+	def _setBasePrediction(self, bp):
+		self.basePrediction = bp
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import election_objects
 # Main program flow goes here
 
 # TODO: Variables to aggregate data from all iterations
+country = election_objects.CountryData()
 
 numOfIterations = int(input("Choose a number of iterations for the simulation to run. Please enter only an integer value: "))
 for i in range (0, numOfIterations):
-    print('this is a placeholder to get VSC to stop yelling at me')
+    for state in country.states:
+        print(state.name, state.ev, state.basePrediction)
 
 # TODO: Display graphics (graphs, map, etc)
