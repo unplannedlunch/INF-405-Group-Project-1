83fda99e91e1185db7a376cc7870d8f24f52b82b
unplannedlunch <jonah.wolmark@gmail.com>
2020-11-12 22:18:39 -0500

    updated documentation and mildly restructured

--- main.py
+++ main.py
@@ -3,16 +3,13 @@
 
 import election_objects
 
-# Main program flow goes here
-
-# TODO: Variables to aggregate data from all iterations
+# Variables to aggregate data
 country = election_objects.CountryData()
-bidenNationalWins = 0
-trumpNationalWins = 0
-nationalTies = 0
+evsPerIteration = []
 bidenStateWins = {}
 trumpStateWins = {}
 
+# Core program loop
 numOfIterations = int(input("Choose a number of iterations for the simulation to run. Please enter only an integer value: "))
 for i in range (0, numOfIterations):
 	bidenEVs = 0
@@ -20,28 +17,30 @@ for i in range (0, numOfIterations):
 	for state in country.states:
 		winner = state.generateCandidateSelectionWithVariance()
 		if winner == 'Biden':
-			#print(state.name)
 			bidenEVs += state.ev
 			bidenStateWins.update({state.name: bidenStateWins.get(state.name, 0) + 1})
 		elif winner == 'Trump':
-			#print(state.name)
 			trumpEVs += state.ev
 			trumpStateWins.update({state.name: trumpStateWins.get(state.name, 0) + 1})
 		else:
 			print("THIS SHOULD NOT HAPPEN", state.name)
-	#print("biden evs:", bidenEVs, "trump evs:", trumpEVs)
-	if(bidenEVs > trumpEVs):
-		bidenNationalWins += 1
-	elif(trumpEVs > bidenEVs):
-		trumpNationalWins += 1
-	else:
-		nationalTies += 1
+		evsPerIteration.append({'Biden': bidenEVs, 'Trump': trumpEVs})
 
+# Process Data
+bidenNationalWins = 0
+trumpNationalWins = 0
+nationalTies = 0
+
+if(bidenEVs > trumpEVs):
+	bidenNationalWins += 1
+elif(trumpEVs > bidenEVs):
+	trumpNationalWins += 1
+else:
+	nationalTies += 1
+
+# Print results to console
 print(bidenStateWins)
 print(trumpStateWins)
-temp = bidenStateWins.copy()
-temp.update(trumpStateWins)
-print(len(temp), sum(bidenStateWins.values()) + sum(trumpStateWins.values()))
 print(bidenNationalWins, trumpNationalWins, nationalTies)
 
 # TODO: Display graphics (graphs, map, etc)
